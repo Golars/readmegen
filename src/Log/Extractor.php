257e5ec091a4b0394e0f4595de21b704dcde1128
Oleg Shabashov <oleg.shabashov@voodoo-mobile.com>
2015-12-03 12:40:00 +0600

    [-] : as delimiter, [+] preg_quote for message_groups

--- src/Log/Extractor.php
+++ src/Log/Extractor.php
@@ -111,6 +111,6 @@ class Extractor {
      * @return string
      */
     protected function getPattern($keywords) {
-        return '/^('.$keywords.'):/i';
+        return '/^('.preg_quote($keywords).')/i';
     }
 }
